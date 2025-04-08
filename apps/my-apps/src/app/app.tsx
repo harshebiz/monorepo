682f3de187c00a026417f78df4bf0058182b2503
harshebiz <harsh.vekariya@esparkbizmail.com>
2025-04-08 19:35:32 +0530

    fix: commit

--- apps/my-apps/src/app/app.tsx
+++ apps/my-apps/src/app/app.tsx
@@ -1,8 +1,10 @@
 import NxWelcome from './nx-welcome';
+import {version} from '../../../../package.json'
 
 export function App() {
   return (
     <div>
+      { import.meta.env.VITE_APP_VERSION || version || 'hello'}
       <NxWelcome title="my-apps" />
     </div>
   );
